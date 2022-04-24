f907523484499800409dc063d95e83b581c15f02
diego.costa <diego.costa@virtualsistemas.com>
2022-04-24 09:21:58 -0300

    mechido na rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,9 @@ import { HomeComponent } from './home/home.component';
 const appRoutes: Routes = [
     { path: '',redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
-    { path: 'cardapio', component: CardapioComponent },
+    { path: '/', component: HomeComponent },
+  { path: 'cardapio', component: CardapioComponent },
+
 
 ]
 
